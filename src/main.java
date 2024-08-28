adf077466e76290d9c4d03ee6cd665e2e916f771
Breno Andrade <130847772+brenoloa@users.noreply.github.com>
2024-08-28 00:24:10 -0300

    fgfdgf

--- src/main.java
+++ src/main.java
@@ -5,32 +5,36 @@ import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int def_quantidade;
-        System.out.print("Defina a quantidade de alunos.  >>: ");
-        def_quantidade = scan.nextInt();
+        int quantidade_alunos;
+
+        System.out.print("Defina a quantidade de alunos: ");
+        quantidade_alunos = scan.nextInt();
         scan.nextLine();
 
         String[] cabecalho = {"NOME", "N1", "N2", "MÉDIA", "STATUS"};
-        Object[][] dados = new Object[def_quantidade][5];
 
-        for (int i = 0; i < def_quantidade; i++) {
+        String[] nomes = new String[quantidade_alunos];
+        float[][] matrizNOTAS = new float[quantidade_alunos][3];
+        String[] resultado = new String[quantidade_alunos];
+
+        for (int i = 0; i < quantidade_alunos; i++) {
             System.out.print("Digite o nome do aluno: ");
-            dados[i][0] = scan.nextLine();
+            nomes[i] = scan.nextLine();
 
             System.out.print("Digite a nota 1: ");
             float nota1 = scan.nextFloat();
 
             System.out.print("Digite a nota 2: ");
             float nota2 = scan.nextFloat();
-            scan.nextLine(); 
+            scan.nextLine();
 
             float media = (nota1 + nota2) / 2;
-            String status = media >= 6.0f ? "APROVADO" : "REPROVADO";
+            String resultadoFINAL = media >= 6.0f ? "APROVADO" : "REPROVADO";
 
-            dados[i][1] = nota1;
-            dados[i][2] = nota2;
-            dados[i][3] = media;
-            dados[i][4] = status;
+            matrizNOTAS[i][0] = nota1;
+            matrizNOTAS[i][1] = nota2;
+            matrizNOTAS[i][2] = media;
+            resultado[i] = resultadoFINAL;
         }
 
         try (FileWriter writer = new FileWriter("tabela_alunos.html")) {
@@ -40,25 +44,23 @@ public class main {
             writer.write("</head>\n<body>\n");
             writer.write("<h1>Dados dos Alunos</h1>\n");
             writer.write("<table>\n<tr>");
-            for (String cab : cabecalho) {
-                writer.write("<th>" + cab + "</th>");
+
+            for (String coluna : cabecalho) {
+                writer.write("<th>" + coluna + "</th>");
             }
             writer.write("</tr>\n");
 
-            for (Object[] linha : dados) {
+            for (int i = 0; i < quantidade_alunos; i++) {
                 writer.write("<tr>");
-                for (int j = 0; j < linha.length; j++) {
-                    if (j == 4) { 
-                        String status = (String) linha[j];
-                        writer.write("<td class=\"" + (status.equals("APROVADO") ? "aprovado" : "reprovado") + "\">" + status + "</td>");
-                    } else {
-                        writer.write("<td>" + linha[j] + "</td>");
-                    }
-                }
+                writer.write("<td>" + nomes[i] + "</td>");
+                writer.write("<td>" + matrizNOTAS[i][0] + "</td>");
+                writer.write("<td>" + matrizNOTAS[i][1] + "</td>");
+                writer.write("<td>" + matrizNOTAS[i][2] + "</td>");
+                writer.write("<td class=\"" + (resultado[i].equals("APROVADO") ? "aprovado" : "reprovado") + "\">" + resultado[i] + "</td>");
                 writer.write("</tr>\n");
             }
-            writer.write("</table>\n");
 
+            writer.write("</table>\n");
             writer.write("<a href=\"index.html\"><button type=\"submit\">SAIR</button></a>");
             writer.write("</body>\n</html>");
 
